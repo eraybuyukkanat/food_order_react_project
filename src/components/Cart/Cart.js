08fb71eb40250dd10739cb726a253a14389e3e52
eraybuyukkanat <erybykknt@gmail.com>
2023-06-14 00:06:59 +0300

    modaloverlay

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,5 @@
 import styles from "./Cart.module.css";
-
+import Modal from "../UI/Modal";
 const Cart = (props) => {
   const cartItems = (
     <ul className={styles['cart-items']}>
@@ -10,7 +10,7 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
@@ -20,7 +20,7 @@ const Cart = (props) => {
             <button className={styles['button--alt']}>Close</button>
             <button className={styles.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
